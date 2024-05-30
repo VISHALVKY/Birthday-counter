7cb8ee9f5b0e5e0d4c6024132b6770bd5e3748b7
VISHALVKY <vishalvky1510@gmail.com>
2024-05-30 12:33:40 +0530

    Updates

---
+++ precache-manifest.2c62810e88d9532a18fa448ef90c9bf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d74be916cdf16e8034cdbe874aa7e3",
+    "url": "/Birthday-counter/index.html"
+  },
+  {
+    "revision": "5e237be5ab59245e1533",
+    "url": "/Birthday-counter/static/css/main.56f1a7a4.chunk.css"
+  },
+  {
+    "revision": "80c274199a213c528ef1",
+    "url": "/Birthday-counter/static/js/2.5dc2bac3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Birthday-counter/static/js/2.5dc2bac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e237be5ab59245e1533",
+    "url": "/Birthday-counter/static/js/main.7f506f51.chunk.js"
+  },
+  {
+    "revision": "52ed428b2d0e9156acb6",
+    "url": "/Birthday-counter/static/js/runtime-main.ab09431a.js"
+  },
+  {
+    "revision": "90118e04a5ab527cb88749db824d7b7b",
+    "url": "/Birthday-counter/static/media/githubLogo.90118e04.svg"
+  }
+]);
